0e306bc878b2fa83d42f2e58f40adcbc43c8af3e
Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>
2024-04-24 21:44:02 +0200

    Clean up health endpoint
    Signed-off-by: Jan Ehrhardt <59441+jehrhardt@users.noreply.github.com>

--- src/api/health.rs
+++ src/api/health.rs
@@ -1,8 +1,43 @@
 // © Copyright 2024 Jan Ehrhardt
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde_json::{json, Value};
+
+async fn health() -> Json<Value> {
+    Json(json!({ "status": "OK" }))
+}
 
 pub(crate) fn router() -> Router {
-    Router::new().route("/health", get(|| async { "OK" }))
+    Router::new().route("/health", get(health))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{self, Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn test_health() {
+        let response = router()
+            .oneshot(
+                Request::builder()
+                    .uri("/health")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({ "status": "OK" }));
+    }
 }
